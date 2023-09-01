d7f13ae313eca355d934f79685eb2f28164f0b85
morphage32 <mwigand32@yahoo.com>
2023-09-01 15:02:14 -0700

    Added field for each player's king to find out if they are in check or not

--- lib/king.rb
+++ lib/king.rb
@@ -1,10 +1,11 @@
 class King
   attr_reader :name, :color, :symbol
-  attr_accessor :possible_moves
+  attr_accessor :possible_moves, :in_check
   def initialize(color)
     @name = "King"
     @color = color
     color == "white" ? (@symbol = "♚") : (@symbol = "♔")
+    @in_check = false
     @possible_moves = []
   end
 end
