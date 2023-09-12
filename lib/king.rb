dfdd3d90f92f30d9a8717881b67252f6d213bdc4
morphage32 <mwigand32@yahoo.com>
2023-09-11 23:30:50 -0700

    Additional checks added to 'update_moves' function to allow the king to castle when all required conditions are met.
    The king will be able to castle when neither it nor the chosen rook has moved, the space between both pieces are empty, and the king's path is not in check at any point.

--- lib/king.rb
+++ lib/king.rb
@@ -26,5 +26,39 @@ class King
       end
     end
     # add castling moves
+    if @first_move
+      i = position[0]
+      unless current_game.king_in_check?(@color)
+        row = current_game.board[i]
+        if row[0] && row[0].first_move # queenside
+          if row[1].nil? && row[2].nil? && row[3].nil?
+            j = 3
+            check = false
+            while j > 1 do
+              test_board = current_game.build_test_board
+              current_game.move_piece(position, [i, j], test_board)
+              check = current_game.king_in_check?(@color, test_board, [i, j])
+              break if check
+              j -= 1
+            end
+            @possible_moves.push(current_game.array_to_coords([i, j + 1])) unless check
+          end
+        end
+        if row[7] && row[7].first_move # kingside
+          if row[5].nil? && row[6].nil?
+            j = 5
+            check = false
+            while j < 7 do
+              test_board = current_game.build_test_board
+              current_game.move_piece(position, [i, j], test_board)
+              check = current_game.king_in_check?(@color, test_board, [i, j])
+              break if check
+              j += 1
+            end
+            @possible_moves.push(current_game.array_to_coords([i, j - 1])) unless check
+          end
+        end
+      end
+    end
   end
 end
